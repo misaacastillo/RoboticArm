e37faeb28c4dfbf005ebda27867c46ac54012dfc
Misael Castillo <102986218+misaacastillo@users.noreply.github.com>
2024-08-17 15:32:43 -0300

    Create proyect.h

---
+++ include/proyect.h
@@ -0,0 +1,15 @@
+#include "pico/stdlib.h"
+#include <stdio.h>
+
+#define SERVOPIN0 0
+#define SERVOPIN1 1
+#define POTPIN0 26
+#define POTPIN1 27
+
+#define SERVOPIN2 2
+#define SERVOPIN3 3
+#define POTPIN2 28
+#define POTPIN3 29
+
+#define SERVOPIN4 14
+#define GATEPIN 15
