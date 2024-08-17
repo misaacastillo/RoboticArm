46625c80bd287ba1cde669c1b924acaee6301171
Misael Castillo <102986218+misaacastillo@users.noreply.github.com>
2024-08-17 15:34:20 -0300

    Create servo.h

---
+++ lib/servo.h
@@ -0,0 +1,8 @@
+#include "pico/stdlib.h"
+#include <stdio.h>
+
+#define SERVO_MIN 500
+#define SERVO_MAX 2500
+
+void set_servo_angle(uint8_t slicenum ,uint8_t servopin , float angulo); // prototipo
+
