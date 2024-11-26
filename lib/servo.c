6214413b1533cecbd4f9efa04f6715c83536a08f
Misael Castillo <102986218+misaacastillo@users.noreply.github.com>
2024-11-26 18:47:23 -0300

    Delete lib/servo.c

--- lib/servo.c
+++
@@ -1,9 +0,0 @@
-#include "pico/stdlib.h"
-#include <stdio.h>
-#include "servo.h"
-
-void set_servo_angle(uint slicenum , uint servopin , float angulo) {
-
-uint16_t calc_angle = SERVO_MIN + (angulo / 270)*(SERVO_MAX - SERVO_MIN);
-
-}
