ecaf91dbed4f9741c7b3b90c708a8fac4891de18
Misael Castillo <102986218+misaacastillo@users.noreply.github.com>
2024-11-26 18:47:00 -0300

    Delete lib/servo.h

--- lib/servo.h
+++
@@ -1,8 +0,0 @@
-#include "pico/stdlib.h"
-#include <stdio.h>
-
-#define SERVO_MIN 500
-#define SERVO_MAX 2500
-
-void set_servo_angle(uint8_t slicenum ,uint8_t servopin , float angulo); // prototipo
-
